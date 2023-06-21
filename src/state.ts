4be26992dc108ae189aaffb8c54175344403d2ed
Alexis Delage <alexisd@bam.tech>
2023-06-21 16:09:59 +0200

    feat: create setup function with registration of dev menu

---
+++ src/state.ts
@@ -0,0 +1,41 @@
+import { useSyncExternalStore } from 'react';
+
+const createStore = () => {
+  type State = {
+    isEnabled: boolean;
+  };
+
+  let state: State = {
+    isEnabled: false,
+  };
+
+  const setState = (newState: Partial<State>) => {
+    state = { ...state, ...newState };
+
+    for (const listener of listeners) {
+      listener();
+    }
+  };
+
+  const listeners = new Set<() => void>();
+
+  return {
+    toggleIsEnabled: () => {
+      setState({ isEnabled: !state.isEnabled });
+    },
+    subscribe: (callback: () => void) => {
+      listeners.add(callback);
+
+      return () => {
+        listeners.delete(callback);
+      };
+    },
+    getSnapshot: () => state,
+  };
+};
+
+export const store = createStore();
+
+export const useStore = () => {
+  return useSyncExternalStore(store.subscribe, store.getSnapshot);
+};
