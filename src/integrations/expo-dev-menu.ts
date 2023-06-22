59938154c7a2cdfdfad6a0fbc8497929b8b10923
Alexis Delage <alexisd@bam.tech>
2023-06-22 18:03:21 +0200

    fix: make expo-dev-menu really optional

--- src/integrations/expo-dev-menu.ts
+++ src/integrations/expo-dev-menu.ts
@@ -1,16 +1,19 @@
-import * as ExpoDevMenu from 'expo-dev-menu';
-import type { ExpoDevMenuItem } from 'expo-dev-menu/build/ExpoDevMenu.types';
-
 import { store } from '../state';
-
-export const devMenuItem: ExpoDevMenuItem = {
-  name: '📐 Toggle Screen Sizer',
-  callback: () => store.toggleIsEnabled(),
-};
 
 /**
  * NOTE: this will override any other custom items, not good!
  */
-export const registerExpoDevMenuItem = () => {
-  ExpoDevMenu.registerDevMenuItems([devMenuItem]).catch(console.warn);
+export const registerExpoDevMenuItem = async () => {
+  try {
+    const ExpoDevMenu = await import('expo-dev-menu');
+
+    await ExpoDevMenu.registerDevMenuItems([
+      {
+        name: '📐 Toggle Screen Sizer',
+        callback: () => store.toggleIsEnabled(),
+      },
+    ]);
+  } catch (error) {
+    console.log('Error with expo-dev-menu', error);
+  }
 };
