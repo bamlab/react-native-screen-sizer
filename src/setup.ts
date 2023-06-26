d3603a99387c90995229ed383701fe71db17c63a
Paul <paulb@bam.tech>
2023-06-26 15:16:30 +0200

    feat: add setupConfig with attribute activatedByDefault

--- src/setup.ts
+++ src/setup.ts
@@ -1,7 +1,15 @@
 import { registerExpoDevMenuItem } from './integrations/expo-dev-menu';
 import { registerReactNativeDevMenuItem } from './integrations/react-native-dev-menu';
+import { store } from './state';
 
-export const setup = () => {
+type SetupConfig = {
+  activatedByDefault?: boolean;
+};
+
+export const setup = ({ activatedByDefault = false }: SetupConfig) => {
   registerExpoDevMenuItem();
   registerReactNativeDevMenuItem();
+  if (activatedByDefault) {
+    store.toggleIsEnabled();
+  }
 };
